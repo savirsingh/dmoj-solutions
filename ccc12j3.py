d6fddbe200aa08c0a6ebf10152b53ce6760666de
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2023-08-08 22:01:13 -0400

    Add some CCC problems

---
+++ ccc12j3.py
@@ -0,0 +1,12 @@
+# code by savir singh
+
+k = int(input())
+
+for i in range(k):
+    print(k*"*" + k*"x" + k*"*")
+
+for j in range(k):
+    print(k*" " + k*"xx")
+
+for l in range(k):
+    print(k*"*" + k*" " + k*"*")
